d4c17b81693fe162cc87c88b6ed8fd60632086ef
ArthurKrisna <arthurkrisna@student.telkomuniversity.ac.id>
2021-10-24 23:14:36 +0700

    MODUL2

---
+++ MODUL2 ARTHUR/Home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <title>Home</title>
+</head>
+<body>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="collapse navbar-collapse d-flex justify-content-center">
+               <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="Home.php">Home</a>
+                    <a class="nav-item nav-link" href="Booking.php">Booking</a>
+               </div>
+          </div>
+     </nav>
+
+     <div class="title text-center">
+          <h4 class="text-info mt-3 text-dark">WELCOME TO ESD VENUE RESERVATION </h4>
+          
+     </div>
+
+     <div class="container d-flex justify-content-center p-2 mt-5">
+
+          <div class="card mx-5">
+               <img class="card-img-top"
+                    src="Standard.jfif">
+               <div class="card-body">
+               <h4 class="card-title text-left">Place 1<br>
+               <h5><span class="text-muted">$ 1000/Days</span><br>
+               <span class="text-muted"> 300 Capacity</span></h5>
+                    <p class="card-text">
+                         <table class="table text-center ">
+                              <tr>
+                                   <td >NO Free Parking</td>
+                              </tr>
+                              <tr>
+                                   <td>NO Full AC</td>
+                              </tr>
+                              <tr> 
+                                   <td>NO Cleaning Service</td>
+                              </tr>
+                              <tr>
+                                   <td>Covid-19 Protocol</td>
+                              </tr>
+                         </table>
+                    </p>
+               </div>
+                    <div class="card-footer d-flex justify-content-center">
+                         <a class="btn btn-primary" href="Booking.php?room=Standard">Book now</a>
+                    </div>
+          </div>
+
+          <div class="card mx-5">
+               <img class="card-img-top"
+                    src="Superior.jfif">
+               <div class="card-body">
+               <h4 class="card-title text-left">Place 2<br>
+               <h5><span class="text-muted">$ 1500/Days</span><br>
+               <span class="text-muted"> 700 Capacity</span></h5>
+                    <p class="card-text">
+                         <table class="table text-center">
+                              <tr>
+                                   <td> Free Parking</td>
+                              </tr>
+                              <tr>
+                                   <td> Full AC</td>
+                              </tr>
+                              <tr>
+                                   <td> No Cleaning Service</td>
+                              </tr>
+                              <tr>
+                                   <td>Covid-19 Protocol</td>
+                              </tr>
+                            
+                         </table>
+                    </p>
+               </div>
+               <div class="card-footer d-flex justify-content-center">
+                    <a class="btn btn-primary" href="Booking.php?room=Superior">Book now</a>
+               </div>
+          </div>
+
+          <div class="card  mx-5">
+               <img class="card-img-top"
+                    src="Luxury.jfif">
+               <div class="card-body">
+               <h4 class="card-title text-left">Place 3<br>
+               <h5><span class="text-muted">$ 5000/Days</span><br>
+               <span class="text-muted"> 1000 Capacity</span></h5>
+                    <p class="card-text">
+                         <table class="table text-center">
+                              <tr>
+                                   <td>Free Parking</td>
+                              </tr>
+                              <tr>
+                                   <td>Full AC</td>
+                              </tr>
+                              <tr>
+                                   <td>Cleaning Service</td>
+                              </tr>
+                              <tr>
+                                   <td>Covid-19 Protocol</td>
+                              </tr>
+                              
+                         </table>
+                    </p>
+               </div>
+               <div class="card-footer d-flex justify-content-center">
+                    <a class="btn btn-primary" href="Booking.php?room=Luxury">Book now</a>
+               </div>
+          </div>
+     </div>
+     <div class="card-footer text-muted text-center">
+    Dibuat oleh Arthur_1202190020
+  </div>
+</div>
+</body>
+
+</html>
---
+++ MODUL2 ARTHUR/myBooking.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <title>myBooking</title>
+</head>
+
+<body>
+     <?php
+     error_reporting(E_ERROR | E_PARSE);
+     $idNumber = rand(600000, 700000);
+     $nama = $_POST['nama'];
+     $checkIn = $_POST['checkin'];
+     $duration = $_POST['duration'];
+     $checkOut = $checkIn . " + " . $duration . " days";
+     $room = $_POST['room'];
+     $phone = $_POST['nomor'];
+
+     if ($room == "Standard.jfif") :
+          $room = 'Standard';
+     elseif ($room == "Superior.jfif") :
+          $room = 'Superior';
+     elseif ($room == "Luxury.jfif") :
+          $room = 'Luxury';
+     endif;
+
+     if ($room == "Standard") :
+          $total = 1000 * $duration;
+     elseif ($room == "Superior") :
+          $total = 1500 * $duration;
+     elseif ($room == "Luxury") :
+          $total = 5000 * $duration;
+     endif;
+
+     if (empty($_POST['service1']) and empty($_POST['service2']) ) :
+          $serv1 = "-";
+          $serv2 = "-";
+          $servShow = "no service";
+     endif;
+
+     if (!empty($_POST['service1']) and !empty($_POST['service2']) ) :
+          $servShow = "catering & dc";
+          $total += 45;
+     endif;
+
+     if (empty($_POST['service1']) and !empty($_POST['service2'])):
+          $serv1 = "-";
+          $servShow = "dc";
+          $total += 25;
+     endif;
+
+     if (empty($_POST['service2']) and !empty($_POST['service1'])):
+          $serv2 = "-";
+          $servShow = "catering";
+          $total += 20;
+     endif;
+     if (!empty($_POST['service1']) and !empty($_POST['service2']) and !empty($_POST['service3']) ) :
+          $servShow = "catering & dc & ss";
+          $total += 75;
+     endif;
+
+     if (empty($_POST['service1']) and empty($_POST['service2']) and !empty($_POST['service3']) ):
+          $serv3 = "ss";
+          $servShow = "ss";
+          $total += 30;
+     endif;
+
+
+     ?>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="collapse navbar-collapse d-flex justify-content-center">
+               <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="Home.php ">Home</a>
+                    <a class="nav-item nav-link active" href="Booking.php">Booking</a>
+               </div>
+          </div>
+     </nav>
+     <br><br>
+     <div class="container d-flex justify-content-center">
+          <table class="table table-striped">
+               <tr>
+                    <th>Booking Number</th>
+                    <th>Name</th>
+                    <th>Check-in</th>
+                    <th>check-out</th>
+                    <th>Building Type</th>
+                    <th>Phone Number</th>
+                    <th>Service</th>
+                    <th>Total Price</th>
+               </tr>
+               <tr>
+                    <td class="font-weight-bold"><?= $idNumber ?></td>
+                    <td><?= $nama ?></td>
+                    <td><?= date('d/m/Y', strtotime($checkIn)) ?></td>
+                    <td><?= date('d/m/Y', strtotime($checkOut)) ?></td>
+                    <td><?= $room; ?></td>
+                    <td><?= $phone ?></td>
+                    <td><?= $servShow ?></td>
+                    <td>$ <?= $total ?></td>
+               </tr>
+          </table>
+     </div>
+     <div class="card-footer text-muted text-center">
+    dibuat oleh Arthur_1202190020
+  </div>
+</body>
+
+</html>

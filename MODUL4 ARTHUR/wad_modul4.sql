40a5b85120ebde9b97dfd6a274225dfe3b462c2c
ArthurKrisna <arthurkrisna@student.telkomuniversity.ac.id>
2021-11-28 23:24:28 +0700

    TP MODUL 4 MASIH ERROR

--- MODUL4 ARTHUR/wad_modul4.sql
+++ MODUL4 ARTHUR/wad_modul4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2021 at 10:36 AM
+-- Generation Time: Nov 28, 2021 at 05:22 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -41,8 +41,7 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`id`, `user_id`, `nama_tempat`, `lokasi`, `harga`, `tanggal`) VALUES
-(3, 1, 'Tanah Lot', 'Bali', 100000, '2021-11-30'),
-(10, 1, 'Raja Ampat', 'Papua', 300000, '0000-00-00');
+(3, 1, 'Tanah Lot', 'Bali', 100000, '2021-11-30');
 
 -- --------------------------------------------------------
 
@@ -66,7 +65,8 @@ INSERT INTO `users` (`id`, `nama`, `email`, `password`, `no_hp`) VALUES
 (1, 'user', 'user@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '76543245'),
 (2, 'user', 'ok@gmail.com', 'user', 'user'),
 (3, 'user', 'user1@gmail.com', 'user', 'user'),
-(4, 'user', 'user2@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '098765');
+(4, 'user', 'user2@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '098765'),
+(5, 'arthur', 'krisnaartur@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '2131231');
 
 -- --------------------------------------------------------
 
@@ -88,9 +88,9 @@ CREATE TABLE `wisata` (
 --
 
 INSERT INTO `wisata` (`id`, `nama`, `lokasi`, `deskripsi`, `gambar`, `harga`) VALUES
-(1, 'Tanah Lot', 'Bali', 'Tanah Lot salah satu pura penting bagi umat Hindu Bali dan lokasi pura terletak di atas batu besar yang berada di lepas pantai. Pura Tanah Lot merupakan ikon pariwisata pulau Bali.', 'assets/tanahlot.jpg', 100000),
-(2, 'Bromo', 'Malang', 'Pesona Gunung Bromo yang indah tidak hanya terkenal di Indonesia, tetapi juga di banyak negara asing. Padang pasir, savana, dan kawah gunung berapi adalah beberapa panorama ikon Jawa Timur.', 'assets/bromo.jpg', 250000),
-(3, 'Raja Ampat', 'Papua', 'Raja Ampat, sebuah Kabupaten dan merupakan bagian dari Provinsi Papua Barat. Suguhan alam yang ditawarkan begitu mengagumkan, hingga bisa membuat wisatawan enggan pulang.', 'assets/rajaampat.jpg', 300000);
+(1, 'Tanah Lot', 'Bali', 'adalah salah satu Pura (Tempat Ibadah Umat Hindu) yang sangat disucikan di Bali, Indonesia. Di sini ada dua pura yang terletak di atas batu besar. Satu terletak di atas bongkahan batu dan satunya terletak di atas tebing mirip dengan Pura Uluwatu.', 'assets/1.jpg', 700000),
+(2, 'Bromo', 'Malang', 'Gunung Bromo (dari bahasa Sanskerta: Brahma, salah seorang Dewa Utama dalam agama Hindu) atau dalam bahasa Tengger dieja \"Brama\", adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia', 'assets/2.jpg', 500000),
+(3, 'Raja Ampat', 'Papua', 'Kabupaten Raja Ampat adalah salah satu kabupaten di provinsi Papua Barat, Indonesia. Ibukota kabupaten ini terletak di Waisai. Kabupaten ini memiliki 610 pulau, termasuk kepulauan Raja Ampat. Empat', 'assets/3.jpg', 800000);
 
 --
 -- Indexes for dumped tables
@@ -122,13 +122,13 @@ ALTER TABLE `wisata`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `wisata`
